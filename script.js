b7450a2925c0395a35a292156e69397403f24f0f
Pedro Garcia <alex1983mona@yahoo.com>
2023-06-19 20:52:44 -0400

    done99%

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Export all data
-const puppyBowl = require("./puppy_bowl_data");
 
 const playerContainer = document.querySelector("#all-players-container");
 const newPlayerFormContainer = document.querySelector("#new-player-form");
